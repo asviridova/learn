643d2f18411d1e597ebb534f51e63dafb48b8a99
asviridova <asviridova@diasoft.ru>
2019-10-20 10:32:28 +0300

    убрано лишнее

--- sa08/build.bat
+++
@@ -1,2 +0,0 @@
-C:\maven\copy\apache-maven-3.2.5\bin\mvn.bat -Dmaven.test.skip=true clean install 
-#C:\maven\apache-maven-3.2.5\bin\mvn.bat eclipse:eclipse
--- sa08/eclipse.bat
+++
@@ -1,2 +0,0 @@
-#C:\maven\apache-maven-3.2.5\bin\mvn.bat clean install 
-C:\maven\apache-maven-3.2.5\bin\mvn.bat -Dmaven.test.skip=true eclipse:eclipse
