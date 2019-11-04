5a0eb4a0d26235d903ac4c8d53ba551d10d1ba88
asviridova <asviridova@diasoft.ru>
2019-11-04 14:42:05 +0300

    RestController добавлен

--- sa04springboot/src/main/java/ru/otus/spring/rest/BiologyTestController.java
+++ sa04springboot/src/main/java/ru/otus/spring/rest/BiologyTestController.java
@@ -1,21 +1,24 @@
 package ru.otus.spring.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.otus.spring.service.BiologyTestService;
 
-@Controller
+@RestController
 public class BiologyTestController {
     private final BiologyTestService biologyTestService;
 
+    @Autowired
     public BiologyTestController(BiologyTestService biologyTestService){
         this.biologyTestService = biologyTestService;
     }
 
     @GetMapping("/next")
     public String getNextQuestion() {
-        return biologyTestService.getMap().toString();
+        return biologyTestService.getCurrentQuestion();
     }
 
 
