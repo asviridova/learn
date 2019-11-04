4c28d11ac7edafab92ef82deafdc278ced67a474
asviridova <asviridova@diasoft.ru>
2019-11-04 18:11:09 +0300

    С веб-интерфейсом

--- sa04springboot/src/main/java/ru/otus/spring/rest/BiologyTestController.java
+++ sa04springboot/src/main/java/ru/otus/spring/rest/BiologyTestController.java
@@ -3,11 +3,14 @@ package ru.otus.spring.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.otus.spring.service.BiologyTestService;
 
-@RestController
+@Controller
 public class BiologyTestController {
     private final BiologyTestService biologyTestService;
 
@@ -16,9 +19,27 @@ public class BiologyTestController {
         this.biologyTestService = biologyTestService;
     }
 
-    @GetMapping("/next")
-    public String getNextQuestion() {
-        return biologyTestService.getCurrentQuestion();
+
+    @GetMapping("/")
+    public String mainPage(Model model) {
+        model.addAttribute("biologyQuestion", biologyTestService.getCurrentQuestion());
+        return "index";
+    }
+
+
+    @GetMapping("/shownext")
+    public String getNextQuestion(Model model) {
+        return "redirect:/";
+    }
+
+    @PostMapping("/select")
+    public String editBookPage(@RequestParam("point") String point, Model model) {
+        if(!StringUtils.isEmpty(point)){
+            if(!point.equalsIgnoreCase("next")){
+                biologyTestService.checkCurrentAnswer(point);
+            }
+        }
+        return "redirect:/";
     }
 
 
