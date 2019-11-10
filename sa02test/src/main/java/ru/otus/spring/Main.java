463096a42bd40f8576ab8054c81b49c13ad0a27c
asviridova <asviridova@diasoft.ru>
2019-11-10 15:04:34 +0300

    Удалено лишнее, реструктурировано

--- sa02test/src/main/java/ru/otus/spring/Main.java
+++ sa02test/src/main/java/ru/otus/spring/Main.java
@@ -3,11 +3,8 @@ package ru.otus.spring;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.otus.spring.domain.Person;
 import ru.otus.spring.service.BiologyTestService;
 import ru.otus.spring.service.BiologyTestServiceImpl;
-import ru.otus.spring.service.PersonService;
 
 import java.util.Scanner;
 
@@ -16,16 +13,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
-
-        PersonService service = context.getBean(PersonService.class);
-
-        Person ivan = service.getByName("Ivan");
-        System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
-
-        Person vasya = service.getByName("Vasya");
-        System.out.println("name: " + vasya.getName() + " age: " + vasya.getAge());
 
         //-----------------
         BiologyTestService biologyService = context.getBean(BiologyTestService.class);
