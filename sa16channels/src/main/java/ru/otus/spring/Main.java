549ce6594ed80635a4546f0ecac3748379c6fccf
asviridova <asviridova@diasoft.ru>
2019-12-07 19:55:17 +0300

    убрано лишнее

--- sa16channels/src/main/java/ru/otus/spring/Main.java
+++ sa16channels/src/main/java/ru/otus/spring/Main.java
@@ -52,7 +52,7 @@ public class Main {
     }
 
     @Bean
-    public IntegrationFlow cafeFlow() {
+    public IntegrationFlow goodsFlow() {
         return IntegrationFlows.from( "goodsChannel" )
                 .split()
                 .handle( "defineService", "defineProvider" )
@@ -69,37 +69,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         ApplicationContext context = SpringApplication.run(Main.class);
-
-
-        CustomerRepository customerRepository = context.getBean(CustomerRepository.class);
-        System.out.println(customerRepository.findAll());
-
-        GoodsRepository goodsRepository = context.getBean(GoodsRepository.class);
-        System.out.println(goodsRepository.findAll());
-
-        GoodsTypeRepository goodsTypeRepository = context.getBean(GoodsTypeRepository.class);
-        System.out.println(goodsTypeRepository.findAll());
-
-        BrandRepository brandRepository = context.getBean(BrandRepository.class);
-        System.out.println(brandRepository.findAll());
-
-        StoreRepository storeRepository = context.getBean(StoreRepository.class);
-        System.out.println(storeRepository.findAll());
-
-        ProviderRepository providerRepository = context.getBean(ProviderRepository.class);
-        System.out.println(providerRepository.findAll());
-
-        BrandService brandService = context.getBean(BrandService.class);
-        System.out.println(brandService.getAll());
-
-
-        //----
-        GoodsService goodsService = context.getBean(GoodsService.class);
-
-        Long id = goodsService.insert("RT4567", "Куртка синяя", "blue", "44", 1300.45, 1L, 1L, 1L, 1L);
-        System.out.println("id="+id);
-        System.out.println("goodsService.getById="+goodsService.getById(id));
-
 
         //-----Integration
         GoodsDistributor goodsDistributor = context.getBean( GoodsDistributor.class );
